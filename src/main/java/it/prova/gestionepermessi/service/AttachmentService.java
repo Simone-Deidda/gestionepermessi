15aad1b9a11d55d50aa18c5f0ddcb77891bd90ef
Simone-Deidda <deidda.simone94@gmail.com>
2022-06-15 20:33:33 +0200

    Implementate interfacce e classi Service

---
+++ src/main/java/it/prova/gestionepermessi/service/AttachmentService.java
@@ -0,0 +1,5 @@
+package it.prova.gestionepermessi.service;
+
+public interface AttachmentService {
+
+}
---
+++ src/main/java/it/prova/gestionepermessi/service/AttachmentServiceImpl.java
@@ -0,0 +1,8 @@
+package it.prova.gestionepermessi.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AttachmentServiceImpl implements AttachmentService {
+
+}
